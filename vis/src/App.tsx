9246b046acf8bc51ecbe04da84eb4a095c29a68b
Veiga Ferreira de Souza <carolvfs@illinois.edu>
2024-10-24 16:27:56 -0500

    first commit

---
+++ vis/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+// import Routes from './routes/Routes'
+
+function App() {
+  return "MyApp"
+  // return (
+  //   <BrowserRouter>
+  //     <div className="app">
+  //       <Routes/>
+  //     </div>
+  //   </BrowserRouter>
+  // )
+}
+
+export default App;
---
+++ vis/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      
+    }),
+    new Dotenv()
+    
+  ],
+  mode: 'development',
+  devServer: {
+    port: 3000 // Change the port here
+  },
+  externals: {
+    'dotenv': 'commonjs dotenv' // Exclude dotenv from the bundle
+  }
+};
